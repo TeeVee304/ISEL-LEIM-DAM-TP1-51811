4776685e771b28595f84a1df03df275356fa3921
Bruno Pereira <brunonmoura@gmail.com>
2026-03-04 11:59:11 +0000

    Setup

--- kotlin/src/main/kotlin/dam/exer_1/exer_1.kt
+++ kotlin/src/main/kotlin/dam/exer_1/exer_1.kt
@@ -2,7 +2,7 @@ package dam.exer_1
 
 fun main() {
     val a = IntArray(50) { it * it }
-    val b = (0..50).map { it * it }
+    val b = (0..49).map { it * it }
     val c = Array(50) { it * it }
     println(a.joinToString())
     println(b.joinToString())
