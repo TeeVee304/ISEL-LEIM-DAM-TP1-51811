a28f98a21a3e3334614cb6afb3637fd0bb2deb90
Bruno Pereira <brunonmoura@gmail.com>
2026-03-08 12:20:24 +0000

    System Info App

--- android/Sytem Info/app/src/main/java/dam_a51811/systeminfo/MainActivity.kt
+++ android/Sytem Info/app/src/main/java/dam_a51811/systeminfo/MainActivity.kt
@@ -1,6 +1,8 @@
 package dam_a51811.systeminfo
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Extração das propriedades de hardware e software através do objeto android.os.Build.
+        val manufacturer = Build.MANUFACTURER
+        val model = Build.MODEL
+        val brand = Build.BRAND
+        val type = Build.TYPE
+        val user = Build.USER
+        val incremental = Build.VERSION.INCREMENTAL
+        val sdk = Build.VERSION.SDK_INT
+        val board = Build.BOARD
+        val display = Build.DISPLAY
+
+        // Formata a string com recurso aos placeholders (%s, %d) definidos na <string name="system_info">.
+        val formattedInfo = getString(
+            R.string.system_info,
+            manufacturer, model, brand, type, user, incremental, sdk, board, display
+        )
+
+        // Localiza o widget 'MultiLine Tex't e atualiza o conteúdo com a formattedInfo.
+        val infoDisplay = findViewById<EditText>(R.id.editTextTextMultiLine)
+        infoDisplay.setText(formattedInfo)
     }
 }
